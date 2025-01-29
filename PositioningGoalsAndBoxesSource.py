48c6a594c591d04e21ccc7c59ba8c05c93bf2c2c
Matias-RC <matias.rodriguez@cenia.cl>
2025-01-28 21:49:07 -0300

    Update PositioningGoalsAndBoxesSource.py

--- PositioningGoalsAndBoxesSource.py
+++ PositioningGoalsAndBoxesSource.py
@@ -1,4 +1,6 @@
 import math
+import random
+import numpy as np
 
 # Arr : Arrengement of goals and boxes
 # Batch : Set of arrengements
@@ -7,3 +9,11 @@ def NumberOfBatches(grid, c):
     y, x = grid
     return int(math.sqrt(y*x) + c)
 
+def RandomBatch(grid, num_goals):
+    """Generates a batch of random goal-box arrangements"""
+    EmptySpace = np.where(grid == 0)
+    for idx in range(num_goals):
+        goal_position = random.choice(list(zip(EmptySpace[0], EmptySpace[1])))
+        grid[goal_position] = 5
+        EmptySpace = np.where(grid == 0)
+    return grid
