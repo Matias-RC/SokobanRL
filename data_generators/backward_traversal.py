c139e18dd0ffbfa5087f2a67bae839a198199343
hectorjimenez12 <hector.jimenezor@gmail.com>
2025-02-11 19:01:53 -0300

    update backward traversal

--- data_generators/backward_traversal.py
+++ data_generators/backward_traversal.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import random
+from managers.inverse_manager import InversedSokobanManager
+from managers.inverse_manager import Node
 
 
 class BackwardTraversal:
@@ -12,6 +14,14 @@ class BackwardTraversal:
         self.maxBreadth = maximumBreadth
         self.cadence = testsPerSearch
         self.inverseManager = inverseManager
+    
+    def GenerateProbs(self,values):
+        if not values:
+            return []
+        values = np.array(values, dtype=np.float64)
+        exp_values = np.exp(values - np.max(values)) 
+        probabilities = exp_values / np.sum(exp_values) # Normalize to sum to 1
+        return probabilities.tolist()
     
     def generate_examples(self,paths):
         for path in paths:
@@ -28,10 +38,49 @@ class BackwardTraversal:
             states_solution, action_solution = end_node.statesList(), end_node.trajectory()
             terminal, initialState = states_solution[-1], states_solution[0]
             #final_grid_state = self.inverseManager.initializer(initial_grid=task.initial_state,end_node=end_node)
-            backwards_paths = self.inverseManager.backward_traversal_paths(end_node=end_node,
+            backwards_paths = self.backward_traversal_paths(end_node=end_node,
                                                             initial_grid=task.initial_state,
                                                             max_depth=self.maxDepth,
                                                             max_breadth=self.maxBreadth,)
             print("OK")
             dataset.append(self.generate())
         return dataset
+    
+    def backward_traversal_paths(self,end_node,initial_grid,max_depth,max_breadth):
+        '''Generates the backward traversal paths from the end node.'''
+        final_grid_state = self.inverseManager.initializer(initial_grid=initial_grid,end_node=end_node)
+
+        self.frontier = []
+        self.frontier.append(end_node)
+        self.seen_states = set()
+
+        while len(self.frontier) > 0 and max_depth > 0:
+            if len(self.frontier) < max_breadth:
+                new_frontier = []
+                for node in self.frontier:
+                    if node.state not in self.seen_states:
+                        self.seen_states.add(node.state)
+                        position_player, position_boxes = node.state
+                        bool_condition, legalActions_boxArrengements = self.inverseManager.legalInverts(posPlayer=position_player,posBox=position_boxes) 
+                        legalActions, boxArrengements = legalActions_boxArrengements
+                        if bool_condition:
+                            for action,box_arr in zip(legalActions,boxArrengements):
+                                new_node = Node(state=(self.inverseManager.FastInvert(position_player,action),box_arr),parent=node,action=action)
+                                if not self.inverseManager.isEndState(new_node.state): #if the state is not the end state then save, whe need to move boxes
+                                    new_frontier.append(new_node)
+                self.depth -= 1
+                self.frontier = new_frontier
+            else:
+                batches = self.makeBatches(self.frontier, self.batchSize)
+                selected_nodes = []
+
+                for batch in batches:
+                    states_batch = [node.state for node in batch]
+                    q_values = random.choices(range(len(states_batch)),k=len(states_batch))
+                    probs = self.GenerateProbs(q_values)
+                    selected_indices = random.choices(range(len(batch)), weights=probs, k=self.drawSize)
+                    for idx in selected_indices:
+                        selected_nodes.append(batch[idx])
+                        
+                self.frontier = selected_nodes
+        print("OK")
