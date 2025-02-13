0d48b2076bf928cc4717baf19a9bf18403f7e4d4
Matias-RC <matias.rodriguez@cenia.cl>
2025-02-13 10:55:48 -0300

    Update backward_traversal.py

--- data_generators/backward_traversal.py
+++ data_generators/backward_traversal.py
@@ -198,12 +198,10 @@ class BackwardTraversal:
                         legalActions, boxArrengements = legalActions_boxArrengements
                         if bool_condition:
                             for action,box_arr in zip(legalActions,boxArrengements):
-                                inversed_action = (-action[0], -action[1])
                                 condition, new_node = self.inverseManager.legalInvertedUpdate(macro=action,
                                                                                             game_data=(position_player,box_arr),
                                                                                             node=node)
-                                
-                                if not self.inverseManager.isEndState(new_node): #if the state is not the end state then save, whe need to move boxes
+                                if condition:
                                     new_frontier.append(new_node)
                 max_depth -= 1
                 frontier = new_frontier
